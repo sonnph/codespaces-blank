a155926d73fd18df07a7ba3b2e343092836d5c86
sonnph <sonnphfx17088@funix.edu.vn>
2024-10-29 14:15:47 +0000

    https://github.com/sonnph/codespaces-blank.git

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-const http
+// Import thư viện Express
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+const router1 = express.Router();
+const router2 = express.Router();
+
+// Định nghĩa 1 route GET cơ bản
+app.get('/', (req, res) => {
+    res.send('Xin chào từ server Node.js!');
+});
+
+app.use('/static', express.static(path.join(__dirname, 'view')));
+
+//Lắng nghe kết nối tại port
+app.listen(port, () => {
+    console.log(`Server đang hoạt động tại http://localhost:${port}`);
+})
